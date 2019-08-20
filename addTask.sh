27922f6ee08f6a297b1cc0126ff4df0f95ba852c
duanshuai <duanbixing@163.com>
2019-08-20 16:09:41 +0800

    删除原来的目录结构

--- addTask.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo "add task to crontab..."
-echo "*/1 * * * * export DISPLAY=:0 && /bin/bash ${DIR}/crontab_watch.sh" | crontab
