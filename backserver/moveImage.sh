6a9e2535e4428ad1bb03deea7571bfaffb1b8c7f
duanshuai <duanshuai@frogshealth.com>
2020-05-28 10:02:29 +0800

    修改ws协议文档格式,删除moveImage.sh中的log信息,删除server.log文件,修改gitignore

--- backserver/moveImage.sh
+++ backserver/moveImage.sh
@@ -30,15 +30,26 @@ do
     #截取从左侧最先匹配到的ImageSavePath/右边的内容
     dir=`echo ${dir#*${ImageSavePath}/}`
     imagename=`echo ${val##*/}`
-    #echo "dir=${dir}, name=${imagename}"
+    #echo "dir=${dir}, name=${imagename}" >> ${root_dir}/${DEBUG}
     
-    if [ ! -d "${root_dir}/${UPLOAD_PATH}/${dir}" ]
+    ymd=$(echo $imagename | awk -F"-" '{print $5}')   
+    #echo "ymd=$ymd" >> ${root_dir}/${DEBUG}
+    ymd=${ymd%%_*}
+    #echo "ymd=$ymd" >> ${root_dir}/${DEBUG}
+    ymd=${ymd:0:8}
+    #echo "ymd=$ymd" >> ${root_dir}/${DEBUG}
+    year=${ymd:0:4}
+    month=${ymd:4:2}
+    day=${ymd:6:2}
+    #echo "year=$year, month=$month, day=$day" >> ${root_dir}/${DEBUG}
+
+    if [ ! -d "${root_dir}/${UPLOAD_PATH}/${dir}/${year}/${month}/${day}" ]
     then
-        ${MKDIR} -p ${root_dir}/${UPLOAD_PATH}/${dir}
+        ${MKDIR} -p ${root_dir}/${UPLOAD_PATH}/${dir}/${year}/${month}/${day}
     fi
 
-    echo "${tmp_time}: ${MV} ${val} ${root_dir}/${UPLOAD_PATH}/${dir}" >> ${root_dir}/${DEBUG}
-    ${MV} ${val} ${root_dir}/${UPLOAD_PATH}/${dir}
+    echo "${tmp_time}: ${MV} ${val} ${root_dir}/${UPLOAD_PATH}/${dir}/${year}/${month}/${day}" >> ${root_dir}/${DEBUG}
+    ${MV} ${val} ${root_dir}/${UPLOAD_PATH}/${dir}/${year}/${month}/${day}
     #sy-wanda-1-9-20190730_180017-696_xxx_xxxxx.jpg 已识别
     #sy-wanda-1-9-20190730_180017-696.jpg   未识别
 done
